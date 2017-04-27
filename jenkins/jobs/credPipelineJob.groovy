39cb49cdc7c9631d4865c50848c1a22b0df6bdb9
Joseph Callen <jcallen@redhat.com>
2017-04-27 13:49:39 -0400

    Jenkins - updates, S2I and test-app run
    Adds update from [ojalloy](https://github.com/jcpowermac/ojalloy) project:
    - Use GitHub SDK
    - The ability to test multiple Dockerfile in various directories
    - Configurations of Jenkins:
      - URL
      - ConfigMap -> Job parameters
    - Utils and DSL updates
    
    Adds the ability to build S2I from the `test/test-app` Ansible playbook and
    start it within a pod.

--- jenkins/jobs/credPipelineJob.groovy
+++ jenkins/jobs/credPipelineJob.groovy
@@ -5,7 +5,26 @@ import com.redhat.*
 
 node {
     def id = null
+    def seedJobParameters = null
     def utils = new Utils()
+
+    /* The Jenkins root url is configured under
+     * Manage Jenkins -> Configure System -> Jenkins Location
+     * This is not configured properly in a ephemeral deployment.
+     * Use the OpenShift route object to determine the correct url.
+     */
+    stage('Configure URL') {
+        openshift.withCluster() {
+            def route = openshift.selector('route', 'jenkins').object()
+            utils.configureRootUrl("https://${route.spec.host}")
+        }
+    }
+    stage('Extract ConfigMap') {
+        openshift.withCluster() {
+            def configMap = openshift.selector( "configmap/orgfolder" ).object().data
+            seedJobParameters = utils.createJobParameters(configMap)
+        }
+    }
 
     stage('OpenShift -> Jenkins credentials') {
         openshift.withCluster() {
@@ -14,6 +33,7 @@ node {
         }
     }
     stage('Run Seed Job') {            
-        build job: 'seed', parameters: [[$class: 'StringParameterValue', name: 'CRED_ID', value: id]]
+        build job: 'seed', parameters: seedJobParameters
     }
+
 }
